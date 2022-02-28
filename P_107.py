ff5ae9cea5fc8fb453fca1938fb39d766e260a77
Harshil-bit <75490038+Harshil-bit@users.noreply.github.com>
2022-02-28 09:58:30 +0530

    Add files via upload

---
+++ P_107.py
@@ -0,0 +1,13 @@
+import plotly.graph_objects as go
+import pandas as pd
+import plotly.express as px
+
+df=pd.read_csv("py/Student_vs_Levels.csv")
+print(df.groupby("level")["attempt"].mean())
+
+fig=go.Figure(go.Scatter(
+            x=df.groupby("level")["attempt"].mean(),
+            y=["Level 1","Level 2","Level 3", "Level 4"],
+            orientation='h'
+            ))
+fig.show()
